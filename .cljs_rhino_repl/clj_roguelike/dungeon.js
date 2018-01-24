ea18ed7c5c9e904b1bf06e81f8b190531f47b2d5
Kevin <regen@anche.no>
2018-01-24 11:47:28 +0100

    gitignore rhino repl

--- .cljs_rhino_repl/clj_roguelike/dungeon.js
+++
@@ -1,8 +0,0 @@
-// Compiled by ClojureScript 1.9.908 {}
-goog.provide('clj_roguelike.dungeon');
-goog.require('cljs.core');
-clj_roguelike.dungeon.create_area = (function clj_roguelike$dungeon$create_area(size){
-return cljs.core.repeat.call(null,size,new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"face","face",-1356480717),new cljs.core.Keyword(null,"wa","wa",1746133599)], null));
-});
-
-//# sourceMappingURL=dungeon.js.map
