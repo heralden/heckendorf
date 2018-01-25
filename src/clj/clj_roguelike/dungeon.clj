df24f4151a083153cafa6272b9ac8144cbd95772
Kevin <regen@anche.no>
2018-01-25 04:25:48 +0100

    More dungeon generation functions along with pretty-print

--- src/clj/clj_roguelike/dungeon.clj
+++ src/clj/clj_roguelike/dungeon.clj
@@ -1,7 +1,9 @@
-(ns clj-roguelike.dungeon)
+(ns clj-roguelike.dungeon
+  (:require [clj-roguelike.random :refer [rand-range perc-chance]]))
 
 (defn create-area [w h]
   {:width w
+   :height h
    :tiles (vec (repeat (* w h) {:tile :wall}))})
 
 (defn- yx->i [w [y x]]
@@ -22,8 +24,43 @@
     (for [y ys x xs] 
       (yx->i width [y x])))) 
 
+(defn- indexes->tiles [tiles indexes]
+  (map (partial nth tiles) indexes))
+
+(defn- assoc-indexes [indexes tile area]
+  (reduce #(assoc-in %1 [:tiles %2 :tile] tile) area indexes))
+
+(defn- within-boundaries? [w h [y x] area]
+  (and (<= (+ h y) (:height area))
+       (<= (+ w x) (:width area))))
+
+(defn- all-walls? [tiles indexes]
+  (every? #(= :wall (:tile %)) 
+          (indexes->tiles tiles indexes)))
+
 (defn add-room [w h yx area]
   (let [indexes (indexes-rect w h yx (:width area))]
-    (if (every? #(= :wall (:tile %)) (map (partial nth (:tiles area)) indexes))
-      (reduce #(assoc-in %1 [:tiles %2 :tile] :empty) area indexes)
+    (if (and (within-boundaries? w h yx area)
+             (all-walls? (:tiles area) indexes))
+      (assoc-indexes indexes :empty area)
       area)))
+
+(defn- rand-coord [area]
+  [(rand-int (:height area))
+   (rand-int (:width area))])
+
+(defn- random-room [area _]
+  (add-room (rand-range 2 9)
+            (rand-range 2 9)
+            (rand-coord area)
+            area))
+
+(defn generate-dungeon [w h rms]
+  (reduce random-room (create-area w h) (repeat rms nil)))
+
+(defn pretty-print [area]
+  (->> (map :tile (:tiles area))
+       (map #(cond (= % :wall) :#
+                   (= % :empty) :.))
+       (partition (:width area))
+       clojure.pprint/pprint))
