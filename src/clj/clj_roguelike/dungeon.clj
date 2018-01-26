8a926ab4270b89ab8cdc9f15a02ddf3f1cf27bda
Kevin <regen@anche.no>
2018-01-26 06:15:40 +0100

    Use unique id for all tiles in each room

--- src/clj/clj_roguelike/dungeon.clj
+++ src/clj/clj_roguelike/dungeon.clj
@@ -1,6 +1,10 @@
 (ns clj-roguelike.dungeon
   (:require [clj-roguelike.random :refer [rand-range perc-chance]]))
 
+(def current-id (atom 0))
+(defn new-id []
+  (swap! current-id inc))
+
 (defn create-area [w h]
   {:width w
    :height h
@@ -11,9 +15,6 @@
 
 (defn- i->yx [w i]
   [(int (/ i w)) (mod i w)])
-
-(defn- next-id []
-  0)
 
 (defn- coord-range [c d]
   (map #(+ c %) (range 0 d)))
@@ -27,8 +28,13 @@
 (defn- indexes->tiles [tiles indexes]
   (map #(nth tiles % nil) indexes))
 
-(defn- assoc-indexes [indexes tile area]
-  (reduce #(assoc-in %1 [:tiles %2 :tile] tile) area indexes))
+(defn- assoc-room [id area index]
+  (-> area
+      (assoc-in [:tiles index :tile] :empty)
+      (assoc-in [:tiles index :id] id)))
+
+(defn- assoc-room-indexes [indexes id area]
+  (reduce (partial assoc-room id) area indexes))
 
 (defn- within-boundaries? [w h [y x] area]
   (and (<= (+ h y) (:height area))
@@ -43,7 +49,7 @@
         boundary (indexes-rect (+ 2 w) (+ 2 h) (mapv dec yx) (:width area))]
     (if (and (within-boundaries? w h yx area)
              (none-empty? (:tiles area) boundary))
-      (assoc-indexes indexes :empty area)
+      (assoc-room-indexes indexes (new-id) area)
       area)))
 
 (defn- rand-coord [area]
@@ -56,8 +62,11 @@
             (rand-coord area)
             area))
 
-(defn generate-dungeon [w h rms]
-  (reduce random-room (create-area w h) (repeat rms nil)))
+(defn- random-corridors [area]
+  )
+
+(defn generate-dungeon [w h room-attempts]
+  (reduce random-room (create-area w h) (repeat room-attempts nil)))
 
 (defn pretty-print [area]
   (->> (map :tile (:tiles area))
