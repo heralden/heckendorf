0f1e3eb67ff7b7952243a88de0c9a541bc92111f
uosl <regen@anche.no>
2018-04-30 20:04:00 +0200

    Use background-image instead

--- src/cljs/clj_roguelike/views.cljs
+++ src/cljs/clj_roguelike/views.cljs
@@ -3,25 +3,23 @@
             [clj-roguelike.subs :as subs]
             ))
 
-(defn tile->graphic [width index tile]
-  ^{:key index} [:div {:style {:background-color
-                               (if (= (:tile tile) :wall)
-                                 "brown"
-                                 "gray")
-                               :width "20px"
-                               :height "20px"
-                               :flex-basis (str (* (/ 1 width) 100) "%")}}])
+(def ^:const tile-size 16)
 
-(defn tiles [game]
-  (let [tiles (:tiles @game)
-        width (:width @game)]
-    (map-indexed
-      (partial tile->graphic width)
-      tiles)))
+(defn tile->graphic [width index tile]
+  ^{:key index} 
+  [:div {:style {:background-image "url('/images/tileset.png')"
+                 :width (str tile-size "px")
+                 :height (str tile-size "px")
+                 :flex-basis (str (* (/ 1 width) 100) "%")}}])
 
 (defn game-tiles []
-  (let [game (re-frame/subscribe [::subs/game-state])]
-    [:div {:style {:display "flex", :flex-wrap "wrap"}} (tiles game)]))
+  (let [{:keys [tiles width]} @(re-frame/subscribe [::subs/game-state])]
+    [:div {:style {:display "flex", 
+                   :flex-wrap "wrap", 
+                   :width (* tile-size width)}}
+     (map-indexed
+       (partial tile->graphic width)
+       tiles)]))
 
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
