edd82b34cda8dc6c262df76f09a777378edde2f2
uosl <regen@anche.no>
2018-05-01 01:48:15 +0200

    Functionality for picking tiles out of tile set with pixelated scaling

--- src/cljs/clj_roguelike/views.cljs
+++ src/cljs/clj_roguelike/views.cljs
@@ -1,16 +1,55 @@
 (ns clj-roguelike.views
-  (:require [re-frame.core :as re-frame]
-            [clj-roguelike.subs :as subs]
-            ))
+    (:require [re-frame.core :as re-frame]
+              [clj-roguelike.subs :as subs]
+              ))
 
-(def ^:const tile-size 16)
+(def real-tileset-width 128)
+(def real-tile-size 16)
+(def tile-scale 2)
+
+(def tileset-width (* tile-scale real-tileset-width))
+(def tile-size (* tile-scale real-tile-size))
+
+(defn get-tileset-pos [[y x]]
+  (let [offset-tile-size (fn [n] (+ (* n tile-size) tile-size))]
+    (str (if (pos? y)
+           (str "bottom " (offset-tile-size y) "px ")
+           "top 0px ")
+         (if (pos? x)
+           (str "right " (offset-tile-size x) "px")
+           "left 0px"))))
+
+; TODO find out how to specify multiple values for one key
+; What we want here is:
+; image-rendering: -moz-crisp-edges;
+; image-rendering: -webkit-crisp-edges;
+; image-rendering: pixelated;
+; image-rendering: crisp-edges;<Paste>
+(def style-rendering 
+  {:image-rendering "-moz-crisp-edges"})
+
+(def wall-style 
+  (into 
+    style-rendering
+    {:background-image "url('/images/tileset.png')"
+     :background-position (get-tileset-pos [0 3])
+     :background-size (str tileset-width "px")}))
+
+(def empty-style 
+  (into
+    style-rendering
+    {:background-color "white"}))
 
 (defn tile->graphic [width index tile]
   ^{:key index} 
-  [:div {:style {:background-image "url('/images/tileset.png')"
-                 :width (str tile-size "px")
-                 :height (str tile-size "px")
-                 :flex-basis (str (* (/ 1 width) 100) "%")}}])
+  [:div {:style 
+         (into
+           (if (= (:tile tile) :wall)
+             wall-style
+             empty-style)
+           {:width (str tile-size "px")
+            :height (str tile-size "px")
+            :flex-basis (str (* (/ 1 width) 100) "%")})}])
 
 (defn game-tiles []
   (let [{:keys [tiles width]} @(re-frame/subscribe [::subs/game-state])]
