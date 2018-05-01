30ac66d441a1f8fd9a04b0ecc6a942d03fcf0a47
uosl <regen@anche.no>
2018-05-01 22:24:16 +0200

    Ensure yx uniqueness among entities, with helper functions and test

--- src/clj/clj_roguelike/entity.clj
+++ src/clj/clj_roguelike/entity.clj
@@ -2,61 +2,72 @@
     (:require [clj-roguelike.random :refer [rand-range]]
               [clj-roguelike.dungeon :refer [rand-coord-tile]]))
 
+(defn unique-yx [area yxs-set]
+  (let [yx (rand-coord-tile :empty area)]
+    (if (contains? yxs-set yx)
+      (recur area yxs-set)
+      yx)))
+
+(defn entity-with [props m area entities]
+  (->> props
+       (into {:yx (unique-yx area (set (map :yx entities)))})
+       (into m)
+       (conj entities)))
+
 (defmulti gen-entity
-  (fn [m _ _] (:type m)))
+  (fn [m area entities] (:type m)))
 
-(defmethod gen-entity :default [m area entities]
-  (conj entities
-        (into m {:yx (rand-coord-tile :empty area)})))
+(defmethod gen-entity :default [& data]
+  (apply entity-with {} data))
 
-(defmethod gen-entity :player [m area entities]
-  (conj entities
-        (into m {:yx (rand-coord-tile :empty area)
-                 :hp 30
-                 :str 5 ; dmg = (str / wep.spd) + wep.att
-                 :exp 0
-                 :lvl 1
-                 :inventory []
-                 :equipped :none})))
+(defmethod gen-entity :player [& data]
+  (apply entity-with
+         {:hp 30
+          :str 5 ; dmg = (str / wep.spd) + wep.att
+          :exp 0
+          :lvl 1
+          :inventory []
+          :equipped :none}
+         data))
 
-(defmethod gen-entity :monster/spider [m area entities]
-  (conj entities
-        (into m {:yx (rand-coord-tile :empty area)
-                 :hp (rand-range 6 10)
-                 :att (rand-range 2 5)})))
+(defmethod gen-entity :monster/spider [& data]
+  (apply entity-with
+         {:hp (rand-range 6 10)
+          :att (rand-range 2 5)}
+         data))
 
-(defmethod gen-entity :monster/skeleton [m area entities]
-  (conj entities
-        (into m {:yx (rand-coord-tile :empty area)
-                 :hp (rand-range 15 25)
-                 :att (rand-range 4 7)})))
+(defmethod gen-entity :monster/skeleton [& data]
+  (apply entity-with
+         {:hp (rand-range 15 25)
+          :att (rand-range 4 7)}
+         data))
 
-(defmethod gen-entity :monster/zombie [m area entities]
-  (conj entities
-        (into m {:yx (rand-coord-tile :empty area)
-                 :hp (rand-range 20 30)
-                 :att (rand-range 5 10)})))
+(defmethod gen-entity :monster/zombie [& data]
+  (apply entity-with
+         {:hp (rand-range 20 30)
+          :att (rand-range 5 10)}
+         data))
 
-(defmethod gen-entity :monster/ghost [m area entities]
-  (conj entities
-        (into m {:yx (rand-coord-tile :empty area)
-                 :hp (rand-range 50 65)
-                 :att (rand-range 15 20)})))
+(defmethod gen-entity :monster/ghost [& data]
+  (apply entity-with
+         {:hp (rand-range 50 65)
+          :att (rand-range 15 20)}
+         data))
 
-(defmethod gen-entity :monster/grim-reaper [m area entities]
-  (conj entities
-        (into m {:yx (rand-coord-tile :empty area)
-                 :hp (rand-range 100 126)
-                 :att (rand-range 30 50)})))
+(defmethod gen-entity :monster/grim-reaper [& data]
+  (apply entity-with
+         {:hp (rand-range 100 126)
+          :att (rand-range 30 50)}
+         data))
 
-(defmethod gen-entity :monster/drake [m area entities]
-  (conj entities
-        (into m {:yx (rand-coord-tile :empty area)
-                 :hp (rand-range 310 350)
-                 :att (rand-range 40 60)})))
+(defmethod gen-entity :monster/drake [& data]
+  (apply entity-with
+         {:hp (rand-range 310 350)
+          :att (rand-range 40 60)}
+         data))
 
-(defmethod gen-entity :monster/dragon [m area entities]
-  (conj entities
-        (into m {:yx (rand-coord-tile :empty area)
-                 :hp (rand-range 1000 1100)
-                 :att (rand-range 70 110)})))
+(defmethod gen-entity :monster/dragon [& data]
+  (apply entity-with
+         {:hp (rand-range 1000 1100)
+          :att (rand-range 70 110)}
+         data))
--- test/clj_roguelike/entity_test.clj
+++ test/clj_roguelike/entity_test.clj
@@ -1,5 +1,6 @@
 (ns clj-roguelike.entity-test
     (:require [clj-roguelike.dungeon :refer [generate-dungeon yx->m rand-coord-tile]]
+              [clj-roguelike.entity :refer :all]
               [midje.sweet :refer :all]))
 
 (def dung (generate-dungeon 25 25 100))
@@ -8,3 +9,10 @@
   (->> (repeatedly 50 #(rand-coord-tile :empty dung))
        (map #(yx->m % dung))
        (map :tile)) => (take 50 (repeat :empty)))
+
+(fact "It will always assign a unique coordinate to entities"
+  (let [entity-yxs (map :yx
+                        (nth (iterate
+                               (partial gen-entity {:type :monster/spider} dung)
+                               []) 100))]
+    (count (set entity-yxs)) => (count entity-yxs)))
